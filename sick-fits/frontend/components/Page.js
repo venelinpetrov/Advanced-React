bfdc3d2b86fca8501cc7729d244c21e5aad15bef
venelinpetrov <vpetrov33tu@gmail.com>
2019-09-08 14:04:49 +0300

    Add styles

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,15 +1,71 @@
 import React, { Component } from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
+
+const theme = {
+    white: '#FFF',
+    red: '#FF0000',
+    black: '#393939',
+    grey: '#3A3A3A',
+    lightgrey: '#E1E1E1',
+    offWhite: '#EDEDED',
+    maxWidth: '1000px',
+    bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
+injectGlobal `
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+  button {  font-family: 'radnika_next'; }
+`;
+
+const StyledPage = styled.div`
+    background: ${props => props.theme.offWhite};
+    color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+    max-width: ${props => props.theme.maxWidth};
+    margin: 0 auto;
+    padding: 2rem;
+    background: red;
+`;
 
 class Page extends Component {
     render() {
         return (
-            <div>
-                <Meta></Meta>
-                <Header></Header>
-                {this.props.children}
-            </div>
+            <ThemeProvider theme={theme}>
+                <StyledPage>
+                    <Meta></Meta>
+                    <Header></Header>
+                    <Inner>
+                        {this.props.children}
+                    </Inner>
+                </StyledPage>
+            </ThemeProvider>
         );
     }
 }
--- sick-fits/frontend/components/styles/NavStyles.js
+++ sick-fits/frontend/components/styles/NavStyles.js
@@ -18,10 +18,12 @@ const NavStyles = styled.ul`
     background: none;
     border: 0;
     cursor: pointer;
+
     @media (max-width: 700px) {
       font-size: 10px;
       padding: 0 10px;
     }
+
     &:before {
       content: '';
       width: 2px;
@@ -33,6 +35,7 @@ const NavStyles = styled.ul`
       top: 0;
       bottom: 0;
     }
+
     &:after {
       height: 2px;
       background: red;
@@ -45,17 +48,17 @@ const NavStyles = styled.ul`
       left: 50%;
       margin-top: 2rem;
     }
+
     &:hover,
     &:focus {
       outline: none;
+
       &:after {
-        width: calc(100% - 60px);
-      }
-    @media (max-width: 700px) {
         width: calc(100% - 10px);
-    }
+      }
     }
   }
+
   @media (max-width: 1300px) {
     border-top: 1px solid ${props => props.theme.lightgrey};
     width: 100%;
