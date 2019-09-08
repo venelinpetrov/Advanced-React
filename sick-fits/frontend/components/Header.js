e6c57e523042d55e14fb809ed2fe7872aee3bebc
venelinpetrov <vpetrov33tu@gmail.com>
2019-09-08 14:31:39 +0300

    Add loader. Render styles on server

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,6 +1,18 @@
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import styled from 'styled-components';
 import Nav from './Nav';
 import Link from 'next/link';
+
+Router.onRouteChangeStart = () => {
+    NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+    NProgress.done();
+};
+Router.onRouteChangeError = () => {
+    NProgress.done();
+};
 const Logo = styled.h1`
     font-size: 2rem;
     margin-left: 4rem;
