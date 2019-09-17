9e005836e2cade53967115733096c2681187491c
venelinpetrov <vpetrov33tu@gmail.com>
2019-09-17 22:08:33 +0300

    Query items from react

---
+++ sick-fits/frontend/components/Items.js
@@ -0,0 +1,53 @@
+import { Component } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import styled from 'styled-components';
+import Item from '../components/Item';
+
+const Center = styled.div`
+    text-align: center;
+`;
+
+const ItemsList = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 60px;
+    max-width: ${props => props.theme.maxWidth};
+    margin: 0 auto;
+`;
+
+const ALL_ITEMS_QUERY = gql`
+    query ALL_ITEMS_QUERY {
+        items {
+            id
+            title
+            price
+            description
+            image
+            largeImage
+        }
+    }
+`;
+
+export default class Items extends Component {
+    render() {
+        return (
+            <Center>
+                <Query query={ALL_ITEMS_QUERY}>
+                    {({error, loading, data}) => {
+                        if (error)
+                            return <p>{error.message}</p>
+                        if (loading)
+                            return <p>Loading...</p>
+
+                        return (
+                            <ItemsList>
+                                {data.items.map(item => <Item key={item.id} item={item}></Item>)}
+                            </ItemsList>
+                        );
+                    }}
+                </Query>
+            </Center>
+        )
+    }
+}
--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,7 +1,7 @@
+import Items from '../components/Items';
 const Home = () => (
     <div>
-        <p>Home</p>
-
+        <Items />
     </div>
 );
 
